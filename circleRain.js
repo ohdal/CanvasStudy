7cbdc37fb5e612e37057c323d0fd8d642fd13dd7
윤현정 <yoonhj97@naver.com>
2023-07-21 11:14:14 +0900

    feat(dat.GUI): dat.GUI를 활용해 효과 값 편하게 조절하기

--- circleRain.js
+++ circleRain.js
@@ -12,6 +12,40 @@ canvas.width = CANVAS_WIDTH * dpr;
 canvas.height = CANVAS_HEIGHT * dpr;
 ctx.scale(dpr, dpr);
 
+const feGaussianBlur = document.querySelector("feGaussianBlur");
+const feColorMatrix = document.querySelector("feColorMatrix");
+
+const controls = new (function () {
+  this.blurValue = 40;
+  this.alphaChannel = 100;
+  this.alphaOffset = -20;
+  this.acc = 1.03;
+})();
+
+let gui = new dat.GUI();
+
+const f1 = gui.addFolder("Gooey Effect");
+f1.open();
+f1.add(controls, "blurValue", 0, 100).onChange((value) => {
+  feGaussianBlur.setAttribute("stdDeviation", value);
+});
+
+f1.add(controls, "alphaChannel", 1, 200).onChange((value) => {
+  feColorMatrix.setAttribute("values", `1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 ${value} ${controls.alphaOffset}`);
+});
+
+f1.add(controls, "alphaOffset", -40, 40).onChange((value) => {
+  feColorMatrix.setAttribute("values", `1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 ${controls.alphaChannel} ${value}`);
+});
+
+const f2 = gui.addFolder("Particle Property");
+f2.open();
+f2.add(controls, "acc", 1, 1.5, 0.01).onChange((value) => {
+  particles.forEach((particle) => {
+    particle.acc = value;
+  });
+});
+
 class Particle {
   constructor(x, y, radius, vy) {
     this.x = x;
